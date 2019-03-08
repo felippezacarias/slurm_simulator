e6df92631462113e516eff40fa645ae23a1a0e96
Felippe Zacarias <fvzacarias@gmail.com>
2019-03-08 16:58:55 +0100

    Printing more job info on script log.

--- src/plugins/jobcomp/script/jobcomp_script.c
+++ src/plugins/jobcomp/script/jobcomp_script.c
@@ -207,6 +207,16 @@ struct jobcomp_info {
 	char *std_out;
 	char *std_err;
 	uint16_t backfilled;
+	//FEIPPE:added
+	char *memory_nodes;
+	uint32_t mnodes;
+	uint32_t num_tasks;
+	uint16_t ntasks_per_node;
+	uint64_t pn_min_memory;	
+	uint32_t min_cpus;
+	uint32_t min_nodes;
+	uint8_t share_res;	
+	uint16_t cpus_per_task;
 #ifdef HAVE_BG
 	char *connect_type;
 	char *geometry;
@@ -218,6 +228,14 @@ static struct jobcomp_info * _jobcomp_info_create (struct job_record *job)
 {
 	enum job_states state;
 	struct jobcomp_info * j = xmalloc (sizeof (*j));
+	job_resources_t *jobresc = NULL; 
+	struct job_details *details = NULL;
+
+	jobresc = job->job_resrcs;
+	details = job->details;
+
+	debug5("FELIPPE: %s for job %u",__func__,job->job_id);
+
 
 	j->jobid = job->job_id;
 	j->exit_code = job->exit_code;
@@ -281,6 +299,23 @@ static struct jobcomp_info * _jobcomp_info_create (struct job_record *job)
 	j->nodes = xstrdup (job->nodes);
 	j->nprocs = job->total_cpus;
 	j->nnodes = job->node_cnt;
+	//FELIPPE: memory node info and more detail
+	if(jobresc){
+		j->nnodes = job->node_cnt - jobresc->memory_nhosts;
+		j->memory_nodes = xstrdup(jobresc->memory_nodes);
+		j->mnodes = jobresc->memory_nhosts;
+	}
+	
+	if(details){
+		j->num_tasks = details->num_tasks;
+		j->ntasks_per_node = details->ntasks_per_node;
+		j->min_cpus = details->min_cpus;
+		j->min_nodes = details->min_nodes;
+		j->pn_min_memory = details->pn_min_memory;
+		j->cpus_per_task = details->cpus_per_task;
+		j->share_res = details->share_res;
+	}
+
 	j->account = job->account ? xstrdup (job->account) : NULL;
 	if (job->resv_name && job->resv_name[0])
 		j->reservation = xstrdup(job->resv_name);
@@ -331,6 +366,8 @@ static void _jobcomp_info_destroy(void *arg)
 	xfree (j->std_err);
 	xfree (j->user_name);
 	xfree (j->work_dir);
+	xfree (j->memory_nodes);
+
 #ifdef HAVE_BG
 	xfree (j->blockid);
 	xfree (j->connect_type);
@@ -486,6 +523,20 @@ static char ** _create_environment (struct jobcomp_info *job)
 	_env_append (&env, "CONNECT_TYPE", job->connect_type);
 	_env_append (&env, "GEOMETRY",     job->geometry);
 #endif
+
+	//FELIPPE: appending new info
+	_env_append (&env, "MEMORYNODES", job->memory_nodes);
+	_env_append_fmt (&env, "MEMORYNODESCNT","%u", job->mnodes);
+	_env_append_fmt (&env, "REQNUMTASKS","%u", job->num_tasks);
+	_env_append_fmt (&env, "REQTASKSPERNODE","%u", job->ntasks_per_node);
+	_env_append_fmt (&env, "REQMINCPUS","%u", job->min_cpus);
+	_env_append_fmt (&env, "REQMINNODES","%u", job->min_nodes);
+	_env_append_fmt (&env, "REQSHARED", (job->share_res ? "yes" : "no"));
+	_env_append_fmt (&env, "REQCPUPERTASK","%u", job->cpus_per_task);
+	if(job->pn_min_memory & MEM_PER_CPU)
+		_env_append_fmt (&env, "MEMORYPERCPU","%lu", job->pn_min_memory & (~MEM_PER_CPU));
+	else
+		_env_append_fmt (&env, "MEMORYPERNODE","%lu", job->pn_min_memory);
 
 	if ((tz = getenv ("TZ")))
 		_env_append_fmt (&env, "TZ", "%s", tz);
